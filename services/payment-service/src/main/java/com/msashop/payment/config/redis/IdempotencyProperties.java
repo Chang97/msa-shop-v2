0ba263bcc895cb9bd1491553fe876e42aec938d6
Chang97 <ckdrbsaos@naver.com>
2026-03-27 03:39:11 +0900

    미사용 소스 제거

--- services/payment-service/src/main/java/com/msashop/payment/config/redis/IdempotencyProperties.java
+++
@@ -1,8 +0,0 @@
-package com.msashop.payment.config.redis;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@ConfigurationProperties(prefix = "app.idempotency")
-public record IdempotencyProperties(String keyPrefix, long ttlSeconds) {
-}
